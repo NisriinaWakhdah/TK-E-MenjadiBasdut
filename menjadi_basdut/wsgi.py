8df15feeef59d29fc34f56fd32615b8a06726c52
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-04-24 20:39:30 +0700

    Initialize Project

---
+++ menjadi_basdut/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for menjadi_basdut project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menjadi_basdut.settings')
+
+application = get_wsgi_application()
