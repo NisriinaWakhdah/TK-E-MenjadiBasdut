8fd2c4cc43a627e5adcdf725a70d5578b4c7559c
sahilaathia <athiasahila@gmail.com>
2026-04-26 13:24:59 +0700

    change project name

--- tiktaktuk/urls.py
+++ tiktaktuk/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for menjadi_basdut project.
+URL configuration for tiktaktuk project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
--- tiktaktuk/wsgi.py
+++ tiktaktuk/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for menjadi_basdut project.
+WSGI config for tiktaktuk project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menjadi_basdut.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tiktaktuk.settings')
 
 application = get_wsgi_application()
