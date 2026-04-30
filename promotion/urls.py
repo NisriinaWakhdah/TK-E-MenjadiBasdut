cc662a9118b9ccbc1ac70c1c45c3a0cef019e435
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-04-30 19:39:47 +0700

    Fixing routing order n promotion

--- promotion/urls.py
+++ promotion/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('promotionList/', views.promotion_management, name='promotion-all'),
-    path('adminPromotionList/', views.admin_promotion_management, name='admin-promotion-list'),
+    path('promotion-list/', views.promotion_management, name='promotion-all'),
+    path('admin-promotion-list/', views.admin_promotion_management, name='admin-promotion-list'),
 ]
