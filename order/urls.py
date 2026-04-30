4f1420d95b4f476802266a75a9b8d43980175d19
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-04-30 07:37:32 +0700

    Implememt FE for organizer's order list and refactor customer's

--- order/urls.py
+++ order/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('checkout/customer', views.checkout_view, name='checkout'),
-    path('orders/customer', views.orders_view, name='orders'),
+    path('orders/customer', views.customer_orders_view, name='orders-customer'),
+    path('orders/organizer', views.organizer_orders_view, name='orders-organizer'),
 ]
--- order/views.py
+++ order/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # These are temporary dummy views to support front-end implementation
 def checkout_view(request):
+
+    # Dummy data
     seats = ["A1", "A2", "A3", "A4", "A5", "B1", "B2", "B3", "B4", "B5", "C1", "C2"]
 
     context = {
@@ -13,7 +15,7 @@ def checkout_view(request):
     }
     return render(request, 'checkout/customer_checkout.html', context)
 
-def orders_view(request):
+def customer_orders_view(request):
 
     # Data dummy untuk simulasi database
     orders = [
@@ -43,4 +45,21 @@ def orders_view(request):
 
     return render(request, 'orders/customer.html', context)
 
-# salah branch
+def organizer_orders_view(request):
+
+    #Dummy data
+    orders = [
+        {'order_id': 'ord_001', 'pelanggan': 'Budi Santoso', 'tanggal': '2024-04-10 14:32', 'status': 'LUNAS', 'total': '1.200.000'},
+        {'order_id': 'ord_002', 'pelanggan': 'Budi Santoso', 'tanggal': '2024-04-11 09:15', 'status': 'LUNAS', 'total': '150.000'},
+        {'order_id': 'ord_003', 'pelanggan': 'Siti Rahayu', 'tanggal': '2024-04-12 18:44', 'status': 'PENDING', 'total': '1.500.000'},
+    ]
+
+    context = {
+        'orders': orders,
+        'total_order': 3,
+        'lunas_count': 2,
+        'pending_count': 1,
+        'total_revenue': '1.350.000',
+        'title': 'Dashboard Organizer - Daftar Order'
+    }
+    return render(request, 'orders/organizer.html', context)
